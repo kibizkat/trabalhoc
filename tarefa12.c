7b707d5649f985f8a07b246f098138606187f636
kibizkat <131921494+kibizkat@users.noreply.github.com>
2023-07-30 20:10:36 -0300

    tarefa12.c
    #patch de correcao de erros XD

--- tarefa12.c
+++ tarefa12.c
@@ -7,8 +7,8 @@ int main(){
     float peso [10];
     float altura [10];
     int media = 0;
-    int cont2 = 0;
-    int cont2 = 0;
+    int a = 0;
+    int b = 0;
 
     for(int i = 0; i < 10; i++){
         printf("Digite a sua idade: \n");
@@ -25,16 +25,18 @@ int main(){
                 printf("A idade media e de: %.d anos \n", media);
                 }
 
-                    if ( i == 9 && (peso[i] > 90 && altura[i] > 1.50)){
-                    cont = 1 + cont;
-                    printf("Contagem de pessoas acima do peso: %d\n", cont);
-                    }
+                    if ((peso[i] > 90 && altura[i] > 150)){
+                    a = 1 + a;
+                    if(i == 9){
+                    printf("Contagem de pessoas acima do peso: %d\n", a);
+                    }}
 
-                        if (i == 9 && (idade[i] >= 10 && idade[i]< 31) && (altura[i] >= 1.90)){
-                        cont2 = 1 + cont2;
-                        int porcent = (cont2 / 10)*100;
-                        printf("%d porcento de pessoas altas entre 10 e 30 anos\n",porcent);
-                        }
+                        if ((idade[i] >= 10 && idade[i]< 31) && (altura[i] >= 190)){
+                        b = 1 + b;
+                        if(i == 9){
+                        float porcent = (b * 100)/10;
+                        printf("%.1f porcento de pessoas altas entre 10 e 30 anos\n",porcent);
+                        }}
             
     }
 }
